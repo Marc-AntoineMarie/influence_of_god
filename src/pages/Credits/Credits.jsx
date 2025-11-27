58c4a81cd7ebb3ec3319c52167f706c2577fb0e6
Marc-AntoineMarie <mmarie.galaxie@gmail.com>
2025-11-27 10:24:43 +0100

    adding credit

--- src/pages/Credits/Credits.jsx
+++ src/pages/Credits/Credits.jsx
@@ -6,10 +6,58 @@ const Credits = () => {
     <main className="credits-page">
       <div className="container">
         <h1>Credits</h1>
-        <p>Design: Lorem ipsum</p>
-        <p src="www.linkedin.com/in/marc-antoine-marie-6a132820a">dev: Marie Marc-Antoine</p>
-        <p>Illustrations: Lorem ipsum</p>
-        <p>Thanks to contributors and playtesters.</p>
+
+        <div className="credit-section">
+          <span className="credit-label">Marketing</span>
+          <a 
+            href="https://linktr.ee/ChloeHeurtebise" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="credit-link"
+          >
+            Chloé Heurtebise
+          </a>
+        </div>
+
+        <div className="credit-section">
+          <span className="credit-label">Marketing</span>
+          <a 
+            href="https://www.linkedin.com/in/emma-foli-54618922a/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="credit-link"
+          >
+            Emma Foli
+          </a>
+        </div>
+        
+        <div className="credit-section">
+          <span className="credit-label">Development</span>
+          <a 
+            href="https://www.linkedin.com/in/marc-antoine-marie-6a132820a" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="credit-link"
+          >
+            Marie Marc-Antoine
+          </a>
+        </div>
+        
+         <div className="credit-section">
+          <span className="credit-label">Development</span>
+          <a 
+            href="https://www.linkedin.com/in/marc-antoine-marie-6a132820a" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="credit-link"
+          >
+            Clara Duponcheli
+          </a>
+        </div>
+
+        <div className="thanks-section">
+          <p>Thanks to contributors and playtesters.</p>
+        </div>
       </div>
     </main>
   );
